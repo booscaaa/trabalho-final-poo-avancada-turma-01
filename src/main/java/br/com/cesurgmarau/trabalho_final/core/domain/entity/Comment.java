e9a229084ac61c3ee27589b2c8c1f6acb3b63d1a
Alex Batista <alexbatistaa3@gmail.com>
2025-05-26 07:51:53 -0300

    :sparkles: feat:Adiciona Get and Setters do Product, Comment, Users

--- src/main/java/br/com/cesurgmarau/trabalho_final/core/domain/entity/Comment.java
+++ src/main/java/br/com/cesurgmarau/trabalho_final/core/domain/entity/Comment.java
@@ -1,4 +1,42 @@
 package br.com.cesurgmarau.trabalho_final.core.domain.entity;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+
+@Entity(name="comment")
 public class Comment {
+    @Id
+    @Column(name = "id")
+    private int id;
+
+    @Column(name = "text")
+    private String text;
+
+    @Column(name = "feeling")
+    private String feeling;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public void setTexto(String text) {
+        this.text = text;
+    }
+
+    public String getFeeling() {
+        return feeling;
+    }
+
+    public void setFeeling(String feeling) {
+        this.feeling = feeling;
+    }
 }
--- src/main/java/br/com/cesurgmarau/trabalho_final/core/domain/entity/User.java
+++ src/main/java/br/com/cesurgmarau/trabalho_final/core/domain/entity/User.java
@@ -1,4 +1,42 @@
 package br.com.cesurgmarau.trabalho_final.core.domain.entity;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+
+@Entity(name="user")
 public class User {
+    @Id
+    @Column(name = "id")
+    private int id;
+
+    @Column(name = "name")
+    private String name;
+
+    @Column(name = "age")
+    private String age;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAge() {
+        return age;
+    }
+
+    public void setAge(String age) {
+        this.age = age;
+    }
 }
