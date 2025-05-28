7a872c1e6825d06d9455f989ec486cf1666f3f2f
cassiano-richato <cassianorichato@cesurg.com>
2025-05-27 21:43:28 -0300

    adicao das dto

--- src/main/java/br/com/cesurgmarau/trabalho_final/core/dto/ComentarioRequest.java
+++ src/main/java/br/com/cesurgmarau/trabalho_final/core/dto/ComentarioRequest.java
@@ -1,4 +1,33 @@
 package br.com.cesurgmarau.trabalho_final.core.dto;
 
 public class ComentarioRequest {
+    private String texto;
+    private Integer usuarioId;
+    private Integer produtoId;
+
+    public ComentarioRequest() {}
+
+    public String getTexto() {
+        return texto;
+    }
+
+    public void setTexto(String texto) {
+        this.texto = texto;
+    }
+
+    public Integer getUsuarioId() {
+        return usuarioId;
+    }
+
+    public void setUsuarioId(Integer usuarioId) {
+        this.usuarioId = usuarioId;
+    }
+
+    public Integer getProdutoId() {
+        return produtoId;
+    }
+
+    public void setProdutoId(Integer produtoId) {
+        this.produtoId = produtoId;
+    }
 }
--- src/main/java/br/com/cesurgmarau/trabalho_final/core/dto/ComentarioResponse.java
+++ src/main/java/br/com/cesurgmarau/trabalho_final/core/dto/ComentarioResponse.java
@@ -1,4 +1,51 @@
 package br.com.cesurgmarau.trabalho_final.core.dto;
 
 public class ComentarioResponse {
+    private Integer id;
+    private String texto;
+    private String sentimento;
+    private Integer usuarioId;
+    private Integer produtoId;
+
+    public ComentarioResponse() {}
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getTexto() {
+        return texto;
+    }
+
+    public void setTexto(String texto) {
+        this.texto = texto;
+    }
+
+    public String getSentimento() {
+        return sentimento;
+    }
+
+    public void setSentimento(String sentimento) {
+        this.sentimento = sentimento;
+    }
+
+    public Integer getUsuarioId() {
+        return usuarioId;
+    }
+
+    public void setUsuarioId(Integer usuarioId) {
+        this.usuarioId = usuarioId;
+    }
+
+    public Integer getProdutoId() {
+        return produtoId;
+    }
+
+    public void setProdutoId(Integer produtoId) {
+        this.produtoId = produtoId;
+    }
 }
