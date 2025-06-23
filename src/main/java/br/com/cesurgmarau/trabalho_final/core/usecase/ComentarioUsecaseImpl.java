6356af3f3dfef7c9f7fd3ec57cf32e44ab2d0fea
cassiano <cassianorichato@gmail.com>
2025-06-23 14:03:51 -0300

    Corrigi a conversao de pontuacao, agora por sql, e removi código não utilizado

--- src/main/java/br/com/cesurgmarau/trabalho_final/core/usecase/ComentarioUsecaseImpl.java
+++ src/main/java/br/com/cesurgmarau/trabalho_final/core/usecase/ComentarioUsecaseImpl.java
@@ -7,7 +7,6 @@ import br.com.cesurgmarau.trabalho_final.infra.ai.OpenAiService;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
-import java.util.Optional;
 
 @Service
 public class ComentarioUsecaseImpl implements ComentarioUsecase {
--- src/main/java/br/com/cesurgmarau/trabalho_final/infra/repository/RelatorioRepositoryImpl.java
+++ src/main/java/br/com/cesurgmarau/trabalho_final/infra/repository/RelatorioRepositoryImpl.java
@@ -9,6 +9,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
 import jakarta.persistence.EntityManager;
+import java.math.BigDecimal;
 import java.util.List;
 
 @Repository
@@ -42,21 +43,28 @@ public class RelatorioRepositoryImpl implements RelatorioRepository {
     @Override
     public List<RelatorioProdutoResponse> mediaSentimentoPorProduto() {
         String query = """
-            SELECT produto_id, AVG(
-                    CASE
-                        WHEN LOWER(sentimento) = 'positivo' THEN 1
-                        WHEN LOWER(sentimento) = 'negativo' THEN -1
-                        ELSE 0
-                    END
-                ) 
-            FROM comentarios 
+            SELECT produto_id,
+                   AVG(
+                       CASE LOWER(sentimento)
+                           WHEN 'muito negativo' THEN 1
+                           WHEN 'negativo' THEN 2
+                           WHEN 'neutro' THEN 3
+                           WHEN 'positivo' THEN 4
+                           WHEN 'muito positivo' THEN 5
+                           ELSE 3
+                       END
+                   ) AS media_pontuacao
+            FROM comentarios
             GROUP BY produto_id
         """;
 
         List<Object[]> resultado = entityManager.createNativeQuery(query).getResultList();
 
         return resultado.stream()
-                .map(entry -> new RelatorioProdutoResponse((Integer) entry[0], (Double) entry[1]))
+                .map(entry -> new RelatorioProdutoResponse(
+                        (Integer) entry[0],
+                        ((BigDecimal) entry[1]).doubleValue()
+                ))
                 .toList();
     }
 
